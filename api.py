7d4da313ae33c3d92a780f0a4a085ecd1344a661
Ebu <ebu@iBoo.lan>
2020-12-23 14:47:31 +0100

    create the flask api and html files

---
+++ api.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+#@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/status')   #get
+def status():
+    return render_template('status.html')
+
+@app.route('/login')    #post
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+   app.run(debug=True)

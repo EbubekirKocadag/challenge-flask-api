a3f7e37b34dcf08a63c822fbd3ab389989b4edfc
Ebu <ebu@iBoo.lan>
2021-01-04 16:11:47 +0100

    css file added, a lot of design change maded on css file

--- form.py
+++ form.py
@@ -8,6 +8,10 @@ from wtforms.validators import DataRequired, Length
     submit = SubmitField('Sign Up')'''
 
 class LoginForm(FlaskForm):
+
+    '''This function will save in a variable what users write on the field and if what's written is not
+    valid it won't work'''
+
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
